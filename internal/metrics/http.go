1fff84efae300ce2134bb3dbcef373edea1075dd
liweigang <liweiganga@uniontech.com>
2026-05-25 14:36:26 +0800

    add http metrics implmentation part 1

--- internal/metrics/http.go
+++ internal/metrics/http.go
@@ -6,3 +6,11 @@ import (
 	"net/http"
 	"time"
 
+	"uos_network_exporter/config"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type HTTPMetrics struct {
+	*baseMetrics
+	logger   *slog.Logger
+	resolver *net.Resolver
