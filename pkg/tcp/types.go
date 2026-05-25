8e3f997d0d68abccad5da2b037a6631fbaeaa682
liweigang <liweiganga@uniontech.com>
2026-05-25 13:52:53 +0800

    add tcp pakcage with TCPPortReturn struct

---
+++ pkg/tcp/types.go
@@ -0,0 +1,15 @@
+package tcp
+
+import "time"
+
+const defaultTimeout = 5 * time.Second
+
+// TCPPortReturn Calculated results
+type TCPPortReturn struct {
+	Success  bool          `json:"success"`
+	DestAddr string        `json:"dest_address"`
+	DestIp   string        `json:"dest_ip"`
+	DestPort string        `json:"dest_port"`
+	SrcIp    string        `json:"src_ip"`
+	ConTime  time.Duration `json:"connection_time"`
+}
