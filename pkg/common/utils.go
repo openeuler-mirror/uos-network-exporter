78d2b2d11a0aa18925e7018b73b4a8eb7c2e4e6b
liweigang <liweiganga@uniontech.com>
2026-05-25 13:36:56 +0800

    add DNS lookup logic in DestAddrs

--- pkg/common/utils.go
+++ pkg/common/utils.go
@@ -8,3 +8,10 @@ import (
 )
 
 func DestAddrs(ctx context.Context, host string, resolver *net.Resolver, timeout time.Duration) ([]string, error) {
+	ipAddrs := make([]string, 0)
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+	addrs, err := resolver.LookupIPAddr(ctx, host)
+	if err != nil {
+		return nil, fmt.Errorf("resolving target: %v", err)
+	}
