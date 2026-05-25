4cda586c155d6234ba605b1b42dce1b56f06442b
liweigang <liweiganga@uniontech.com>
2026-05-25 14:36:26 +0800

    add http metrics implmentation part 2

--- internal/metrics/http.go
+++ internal/metrics/http.go
@@ -14,3 +14,11 @@ type HTTPMetrics struct {
 	*baseMetrics
 	logger   *slog.Logger
 	resolver *net.Resolver
+}
+
+func NewHTTPMetrics(logger *slog.Logger, resolver *net.Resolver) *HTTPMetrics {
+	base := newBaseMetrics("http")
+	base.addMetric("get_seconds", "HTTP Get Drill Down time in seconds", []string{"name", "target", "type"})
+	base.addMetric("get_content_bytes", "HTTP Get Content Size in bytes", []string{"name", "target"})
+	base.addMetric("get_status", "HTTP Get Status", []string{"name", "target"})
+	base.addMetric("get_targets", "Number of active targets", nil)
