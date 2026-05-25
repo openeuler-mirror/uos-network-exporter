1b2397540c217bab7336a7a62059e88883a3a8d2
liweigang <liweiganga@uniontech.com>
2026-05-25 13:40:58 +0800

    add NewFileRotator contructor

--- pkg/logger/filerotator.go
+++ pkg/logger/filerotator.go
@@ -22,3 +22,19 @@ type FileRotator struct {
 	startTime time.Time
 	keepFiles int
 }
+
+func NewFileRotator(basePath string, maxSize int64, maxAge time.Duration) *FileRotator {
+	dir := filepath.Dir(basePath)
+	_, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, 0750); err != nil {
+			fmt.Printf("Warning: Failed to create the log directory: %v\n", err)
+		}
+	}
+	return &FileRotator{
+		basePath:  basePath,
+		maxSize:   maxSize,
+		maxAge:    maxAge,
+		keepFiles: defaultMaxFiles,
+	}
+}
