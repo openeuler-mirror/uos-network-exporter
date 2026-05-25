40db5b7cc0197dfa2ea1f6992cd0496c752d46df
liweigang <liweiganga@uniontech.com>
2026-05-25 13:57:25 +0800

    add RunMTRConcurrent funcion setup

--- pkg/mtr/mtr_concurrent.go
+++ pkg/mtr/mtr_concurrent.go
@@ -31,3 +31,39 @@ func DefaultConcurrentMTROptions() *ConcurrentMTROptions {
 		Timeout:        30 * time.Second,
 	}
 }
+
+// RunMTRConcurrent execute high-performance concurrent MTR route tracing
+func RunMTRConcurrent(destAddr, srcAddr string, timeout time.Duration, maxHops, count int, options *ConcurrentMTROptions) *MtrResult {
+	if options == nil {
+		options = DefaultConcurrentMTROptions()
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), options.Timeout)
+	defer cancel()
+
+	result := &MtrResult{
+		DestAddr:      destAddr,
+		Hops:          []common.IcmpHop{},
+		HopSummaryMap: make(map[string]*common.IcmpSummary),
+	}
+
+	icmpID := &common.IcmpID{}
+	pid := int(icmpID.Get())
+
+	mtrReturns := make([]*safeReturn, maxHops+1)
+	for i := 1; i < maxHops+1; i++ {
+		mtrReturns[i] = &safeReturn{
+			mtrReturn: &MtrReturn{
+				ttl:       i,
+				host:      "unknown",
+				succSum:   0,
+				success:   false,
+				lastTime:  time.Duration(0),
+				sumTime:   time.Duration(0),
+				bestTime:  time.Duration(0),
+				worstTime: time.Duration(0),
+				avgTime:   time.Duration(0),
+				allTime:   make([]time.Duration, 0, count),
+			},
+		}
+	}
