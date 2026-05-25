e07acdbb9cd82c22d421d5b87a68e09d17cff65a
liweigang <liweiganga@uniontech.com>
2026-05-25 14:36:25 +0800

    add tcp metrics implmentation part 1

--- internal/metrics/tcp.go
+++ internal/metrics/tcp.go
@@ -6,3 +6,11 @@ import (
 	"net"
 	"time"
 
+	"uos_network_exporter/config"
+	"uos_network_exporter/pkg/common"
+	"uos_network_exporter/pkg/tcp"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type TCPMetrics struct {
+	*baseMetrics
